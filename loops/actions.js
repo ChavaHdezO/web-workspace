6be719be549483e5bbbfd641aca7c49f9e55d5b7
chava <chava.490@gmail.com>
2023-05-31 22:44:55 -0600

    Break and Continue

---
+++ loops/actions.js
@@ -0,0 +1,14 @@
+// Break and Continue
+
+let num = 1;
+
+for(i = 1; i<=100, i++;){
+    num*=i;
+    //your code goes here
+    if(num > 10000) {
+        break;
+    }
+}
+
+//generate the result
+console.log(num);
