13cc901c397c10a27f5b0bf6f039ce7b9232ca42
chava <chava.490@gmail.com>
2023-06-18 17:07:27 -0600

    Creating Variables

---
+++ mimo/javascriptBasics/creatingVariables.js
@@ -0,0 +1,11 @@
+// let y const son las palabras reservadas que se utilizan en JavaScript para declarar variables.
+// let and const are the reserved words that we use in JavaScript to declare variables.
+
+let northAmericaCity = "Chicago";
+
+// To difference constants over variables, we can write constant names in uppercase letters and in SNAKE_CASE when the constant name is about two or more words.
+
+const WEST_ISLAND = "Malta";
+
+const greeting = "Hello, World!";
+console.log(greeting);
