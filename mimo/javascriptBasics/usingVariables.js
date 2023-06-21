a8c8953d51f3daae91af32f1de5ae84305cfd909
chava <chava.490@gmail.com>
2023-06-21 13:41:44 -0600

    Mimo - JavaScript - Using Variables

---
+++ mimo/javascriptBasics/usingVariables.js
@@ -0,0 +1,15 @@
+let defaultStatus = "Hi there!";
+let currentStatus = "Watching Nerflix";
+currentStatus = "Relaxing at the beach";
+console.log(currentStatus);
+currentStatus = "Reading";
+console.log(currentStatus);
+currentStatus = defaultStatus;
+console.log(currentStatus);
+console.log("Followers: " + "55");
+
+const label = "Following: " +  "5";
+console.log(label);
+
+const numberOfLikes = 5 * 10;
+console.log("Total number of likes: " + (numberOfLikes + 1));
